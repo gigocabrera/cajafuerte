4a1293829b389c39197868a6022f298c89d25397
Luis Cabrera <gigocabrera@outlook.com>
2020-02-22 20:24:45 -0500

    progress

--- src/app/pages/recent/recent.module.ts
+++ src/app/pages/recent/recent.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { RecentPageRoutingModule } from './recent-routing.module';
-
 import { RecentPage } from './recent.page';
+import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RecentPageRoutingModule
+    RecentPageRoutingModule,
+    TranslateModule
   ],
   declarations: [RecentPage]
 })
--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -38,6 +38,16 @@ const routes: Routes = [
         ]
       },
       {
+        path: 'settings',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../settings/settings.module').then(m => m.SettingsPageModule)
+          }
+        ]
+      },
+      {
         path: '',
         redirectTo: '/tabs/menu',
         pathMatch: 'full'
